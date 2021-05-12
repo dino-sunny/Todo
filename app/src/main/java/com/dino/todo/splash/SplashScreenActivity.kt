6c06fc5c5a69d6cd492256380f7efcc7fb0d7b3a
dinosunny <dino.sunny@cabotsolutions.com>
2021-05-12 19:50:16 +0530

    splash screen timeout added

---
+++ app/src/main/java/com/dino/todo/splash/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.dino.todo.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import com.dino.todo.R
+import com.dino.todo.databinding.ActivitySplashBinding
+import com.dino.todo.home.HomeActivity
+import com.dino.todo.utility.Constants.ConstantVariables.SPLASH_TIME_OUT
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var mDelayHandler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        mDelayHandler = Handler()
+        mDelayHandler.postDelayed(mRunnable, SPLASH_TIME_OUT)
+    }
+
+    private val mRunnable: Runnable = Runnable {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
---
+++ app/src/main/java/com/dino/todo/utility/Constants.kt
@@ -0,0 +1,8 @@
+package com.dino.todo.utility
+
+class Constants {
+
+    object ConstantVariables {
+        const val SPLASH_TIME_OUT : Long = 1500
+    }
+}
