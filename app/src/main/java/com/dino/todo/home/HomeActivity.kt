fea854666bcb22c4ce948cf77f29959178f888c9
dinosunny <dino.sunny@cabotsolutions.com>
2021-05-14 16:31:27 +0530

    ui updated

--- app/src/main/java/com/dino/todo/home/HomeActivity.kt
+++ app/src/main/java/com/dino/todo/home/HomeActivity.kt
@@ -1,13 +1,9 @@
 package com.dino.todo.home
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import android.view.Window
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +11,6 @@ import com.dino.todo.R
 import com.dino.todo.database.Todo
 import com.dino.todo.database.TodoDatabase
 import com.dino.todo.databinding.ActivityHomeBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.gson.Gson
 
 class HomeActivity : AppCompatActivity(),OnTodoClickListener {
@@ -38,6 +33,7 @@ class HomeActivity : AppCompatActivity(),OnTodoClickListener {
         setAdapter()
         setObservers()
     }
+    //Adapter for listing items
     private fun setAdapter() {
         todoAdapter = TodoAdapter(this)
         completedTodoAdapter = TodoAdapter(this)
@@ -46,12 +42,13 @@ class HomeActivity : AppCompatActivity(),OnTodoClickListener {
     }
 
     private fun setObservers(){
+        //Shows pending todos list
         homeViewModel.todo.observe(this, {
             it.let {
                 todoAdapter.submitList(it)
             }
         })
-
+        //Shows completed todos list
         homeViewModel.completedTodo.observe(this, {
             it.let {
                 completedTodoAdapter.submitList(it)
@@ -61,7 +58,7 @@ class HomeActivity : AppCompatActivity(),OnTodoClickListener {
         homeViewModel.eventTodo.observe(this, { clicked ->
             if (clicked) {
                 val fragment = AddTodoFragment()
-                fragment.show(supportFragmentManager, "addTodo")
+                fragment.show(supportFragmentManager, getString(R.string.tag))
                 homeViewModel.onAddTodoClickComplete()
             }
         })
@@ -72,33 +69,38 @@ class HomeActivity : AppCompatActivity(),OnTodoClickListener {
     }
 
     override fun onEditClicked(todo: Todo) {
+        showEditTodo(todo)
+    }
+
+    //Function will load a dialog fragment to edit the item
+    private fun showEditTodo(todo: Todo) {
         val bundle = Bundle()
         val mData = Gson().toJson(todo)
         bundle.putString("TODO_UPDATE",mData.toString())
-
         val fragment = AddTodoFragment()
         fragment.arguments = bundle
-        fragment.show(supportFragmentManager, "addTodo")
+        fragment.show(supportFragmentManager, getString(R.string.tag))
     }
-
+    //Function called when a task is completed
     override fun onDoneClicked(todo: Todo) {
         todo.completed = true
         homeViewModel.updateTodo(todo)
         todoAdapter.notifyDataSetChanged()
     }
 
+    //Custom alert dialog screen to ask confirmation for deleting a task
     private fun showDialog(todo: Todo) {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.delete_confirmation)
-        val yesBtn = dialog.findViewById(R.id.confirm_button) as Button
-        val noBtn = dialog.findViewById(R.id.cancel_button) as Button
-        yesBtn.setOnClickListener {
+        val confirmDeleteButton = dialog.findViewById(R.id.confirm_button) as Button
+        val cancelButton = dialog.findViewById(R.id.cancel_button) as Button
+        confirmDeleteButton.setOnClickListener {
             homeViewModel.deleteTodo(todo)
             dialog.dismiss()
         }
-        noBtn.setOnClickListener { dialog.dismiss() }
+        cancelButton.setOnClickListener { dialog.dismiss() }
         dialog.show()
 
     }
