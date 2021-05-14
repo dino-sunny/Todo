570efccf93e2c604adc6deaec135fa2a6a06181c
dinosunny <dino.sunny@cabotsolutions.com>
2021-05-14 08:33:26 +0530

    notification added

--- app/src/main/java/com/dino/todo/home/AddTodoFragment.kt
+++ app/src/main/java/com/dino/todo/home/AddTodoFragment.kt
@@ -1,40 +1,98 @@
 package com.dino.todo.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context.ALARM_SERVICE
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
 import com.dino.todo.R
 import com.dino.todo.database.Todo
 import com.dino.todo.database.TodoDatabase
 import com.dino.todo.databinding.AddTodoFragmentBinding
 
+
 class AddTodoFragment : DialogFragment() {
     private lateinit var addTodoViewModel: AddTodoViewModel
     private lateinit var binding: AddTodoFragmentBinding
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var notifyPendingIntent: PendingIntent
+    private lateinit var mNotificationManager: NotificationManager
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.add_todo_fragment, container, false)
         val application = requireNotNull(this.activity).application
         val dataSource = TodoDatabase.getInstance(application).todoDatabaseDao
         val viewModelFactory = AddTodoViewModelFactory(dataSource, application)
-        addTodoViewModel = ViewModelProvider(this,viewModelFactory).get(AddTodoViewModel::class.java)
+        addTodoViewModel = ViewModelProvider(this, viewModelFactory).get(AddTodoViewModel::class.java)
         binding.addTodoViewModel = addTodoViewModel
+
+        setUp()
+        createNotificationChannel();
 
         return binding.root
     }
 
+    /**
+     * Creates a Notification channel, for OREO and higher.
+     */
+    private fun createNotificationChannel() {
+
+        // Create a notification manager object.
+        mNotificationManager = (activity!!.getSystemService(NOTIFICATION_SERVICE) as NotificationManager?)!!
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            val notificationChannel = NotificationChannel("primary_notification_channel",
+                    "Stand up notification",
+                    NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Notifies every 15 minutes to " +
+                    "stand up and walk"
+            mNotificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun setUp() {
+        alarmManager = activity!!.getSystemService(ALARM_SERVICE) as AlarmManager
+        val notifyIntent = Intent(context, AlarmReceiver::class.java)
+        notifyPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                notifyIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+
     override fun onStart() {
         super.onStart()
-        dialog?.window?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        dialog?.window?.setLayout(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
         setObservers()
     }
 
@@ -44,11 +102,17 @@ class AddTodoFragment : DialogFragment() {
                 val newTodo = Todo()
                 newTodo.title = binding.titleEditText.text.toString()
                 newTodo.description = binding.descriptionEditText.text.toString()
+                setNotification(newTodo)
                 addTodoViewModel.addTodo(newTodo)
                 addTodoViewModel.onSubmitComplete()
                 dismiss()
             }
         })
+    }
+
+    private fun setNotification(todo: Todo) {
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                todo.date, notifyPendingIntent)
     }
 
 
